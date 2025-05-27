b28c1a13754c498ec8fd2a7334c3a1643ca78b30
Ayeesha208 <91049748+Ayeesha208@users.noreply.github.com>
2025-05-27 13:44:40 +0530

    Update script.js

--- Automation_Test-main/CI_CD_DummyCode/frontend/script.js
+++ Automation_Test-main/CI_CD_DummyCode/frontend/script.js
@@ -1,5 +1,5 @@
 function callBackend() {
-  fetch('http://20.121.56.25:5000/api/message')
+  fetch('/api/message')
     .then(response => response.json())
     .then(data => {
       document.getElementById('response').innerText = data.message;
